2f2357979287fe47e3af9debd9abf97d189b28bf
Sharifyanov Marsel <sharifyanovmarsel@gmail.com>
2024-12-17 22:14:35 +0500

    add userService in Main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,8 +2,9 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
-
 
 public class Main {
     public static void main(String[] args) {
@@ -25,5 +26,24 @@ public class Main {
         System.out.println(userDao.getAllUsers());
         userDao.cleanUsersTable();
         userDao.dropUsersTable();
+
+        // Не до конца понял по заданию следует ли алгоритм работы приложения делать через Dao
+        // или нужно через Srvice делать, поэтому напишу тут такой же код через сервисы ¯\_(ツ)_/¯
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Name2", "LastName2", (byte) 25);
+        userService.saveUser("Name1", "LastName1", (byte) 20);
+        userService.saveUser("Name3", "LastName3", (byte) 31);
+        userService.saveUser("Name4", "LastName4", (byte) 38);
+
+        userService.removeUserById(1);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+        Util.closeConnection();
     }
 }
